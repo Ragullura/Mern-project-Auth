052b63be6a7abac9d0ec03116eeeee30801daf26
Ragullura <ragullura.005@gmail.com>
2024-02-11 16:55:17 +0530

    Create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from  'express';
 import mongoose from 'mongoose';
 import dotenv from  'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -10,10 +11,13 @@ mongoose.connect(process.env.MONGO).then(()=>{
 });
 const  app = express();
 
-app.listen(3001, () => {
-    console.log('Server is running on port 3001 ');
+app.use(express.json());//its for running through  the middleware of json data in request body
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
 
 //Path to api
 
 app.use("/api/user",userRoutes)
+app.use("/api/auth",authRoutes)
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,9 @@
 import express from  'express';
-/* import test from 'node:test'; */
 import {test} from '../controllers/user.controller.js'
 
 const router = express.Router();
 
 // @route   GET api/posts
 // @desc    Get all posts
-router.get('/',test)
+router.get("/",test)
 export default router;
