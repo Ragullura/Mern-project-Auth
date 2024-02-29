e3bad7d2afeecc9a8d506c6494a08abe614c738d
Ragullura <ragullura.005@gmail.com>
2024-02-29 17:40:33 +0530

    Complete Home and About Pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,31 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='px-4 py-12 max-w-2xl mx-auto '>
+      <h1 className='text-3xl 
+       font-bold mb-4 text-slate-700'>About Us</h1>
+       <p className='text-xl  text-cyan-700 '>
+       Welcome to Ant Authentication, where our mission is to pioneer secure 
+       digital experiences through innovative technology.Comprising a dynamic 
+       team of developers passionate about user-centric solutions, we leverage 
+       the MERN stack—MongoDB, Express, React, and Node.js—to craft a seamless 
+       and secure online environment. Our journey began with the vision of 
+       creating a robust authentication platform, empowering users with intuitive 
+       sign-up, log-in, and exclusive access to protected routes. The incorporation 
+       of Redux Toolkit ensures a state-of-the-art state management system, 
+       delivering not only security but also a smooth user experience.
+       </p>
+       <p className='text-xl text-blue-900'>
+       At the heart of Ant Authentication lies a commitment to transparency and 
+       collaboration. Our About page serves as a testament to our dedication to providing 
+       users with a detailed understanding of the platform's inner workings. Dive into the 
+       intricacies of our authentication system, explore the codebase on GitHub, and join 
+       us in shaping the future of secure online interactions. As we continue to evolve, our 
+       focus remains on community engagement and user empowerment. Connect with us through 
+       various channels, and let's embark on this journey together toward a safer and more 
+       user-friendly digital landscape.
+       </p>
+
+    </div>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,39 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='px-4 py-12 max-w-2xl mx-auto '>
+       <h1 className='text-3xl 
+       font-bold mb-4 text-slate-700'>Welcome to our MERN Full-Stack Authentication Platform</h1>
+        <h3 className='text-3xl font-semibold text-slate-500 mb-2'>Introduction</h3>
+          <p className='text-xl text-slate-800 '> Briefly explain the purpose of my website.
+              Highlight the key features and benefits of using my application.
+          </p>
+          <h3 className='text-3xl font-semibold text-slate-500 mb-2'>How It Works:</h3>
+          <p className='text-xl text-slate-800 '>
+          Provide a step-by-step guide on how users can sign up, log in, and access protected routes.
+          Explain the authentication flow and the use of Redux Toolkit in managing state.The back-end
+          is built with Node.js and Express and uses MongoDB as the database.
+          Authentication is implemented using JSON Web Tokens (JWT) 
+          </p>
+        <h1 className='text-3xl font-semibold text-slate-500 mb-2'>Features Spotlight</h1>
+        <p className='text-xl text-orange-900'>
+            Welcome to our MERN full-stack authentication platform, 
+            where seamless user experiences meet robust security. 
+            Our website offers a streamlined authentication process, 
+            empowering users to sign up, log in, and access protected 
+            routes with confidence. Leveraging the power of MongoDB, 
+            Express, React, and Node.js, we've implemented a state-of-the-art 
+            authentication system using Redux Toolkit. The intuitive user 
+            interface guides users through the authentication flow, ensuring 
+            a smooth and secure experience. <span className='text-yellow-800'>Check out our GitHub repository 
+            for a deep dive into the codebase, contribute to the project, 
+            or explore the latest updates. Whether you're a developer or an 
+            end-user, our platform combines functionality with simplicity, 
+            creating a space where authenticated users can enjoy exclusive 
+            access to protected routes. Join us on this journey towards a 
+            secure and user-centric online environment.</span>
+        </p>
+
+    </div>
   )
 }
