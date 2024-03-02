232e6ab1e378a6bd4b4c2f2096b8a4a04e3c3467
Ragullura <ragullura.005@gmail.com>
2024-03-02 16:15:44 +0530

    changed minor mistake in firebase

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -6,7 +6,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey:import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "mern-auth-a546c.firebaseapp.com",
   projectId: "mern-auth-a546c",
   storageBucket: "mern-auth-a546c.appspot.com",
